525d99483d5807938177dd5442d871ac0cbf29a1
Beau Frusetta <beau.frusetta@gmail.com>
2010-11-12 17:51:50 -0700

    Removed autoload database class

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -39,7 +39,7 @@
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('database','migration');
+$autoload['libraries'] = array('migration');
 
 /*
 | -------------------------------------------------------------------
